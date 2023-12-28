b160c6943e884d623e1caba84950d78cc4fbbdf9
Guilherme Costa <guizeracostasilva@outlook.com>
2023-12-28 18:36:25 -0300

    Testes inicias com movimentos possiveis das pecas

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -6,7 +6,7 @@ using tabuleiro;
 
 namespace xadrez_console.tabuleiro
 {
-    internal class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -25,5 +25,8 @@ namespace xadrez_console.tabuleiro
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
     }
 }
